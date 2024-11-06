eeb9851997c6da5cd8734b0e2f3bf1caa9a12643
muttaqienaab <131658040+muttaqienaab@users.noreply.github.com>
2024-11-06 21:44:08 +0700

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,24 +3,23 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 from babel.numbers import format_currency
+
 sns.set(style='dark')
 
-def create_daily_orders_(df):
-    daily_orders_df = df.resameple(rule='D',on='order_date').agg({
+def create_daily_orders_df(df):
+    daily_orders_df = df.resample(rule='D', on='order_date').agg({
         "order_id": "nunique",
         "total_price": "sum"
     })
-
-daily_orders_df = daily_orders_df.reset_index()
-daily_orders_df.rename(columns={
-     "order_id": "order_count",
-    "total_price": "revenue"
-}, inplace=True)
-
-return daily_orders_df
+    daily_orders_df = daily_orders_df.reset_index()
+    daily_orders_df.rename(columns={
+         "order_id": "order_count",
+        "total_price": "revenue"
+    }, inplace=True)
+    return daily_orders_df
 
 def create_sum_order_items_df(df):
-    sum_order_items_df = df.groupby("products_name").quantity_x.sum().sort_values(ascending=False).reset_index()
+    sum_order_items_df = df.groupby("product_name").quantity.sum().sort_values(ascending=False).reset_index()
     return sum_order_items_df
 
 def create_bygender_df(df):
@@ -28,7 +27,6 @@ def create_bygender_df(df):
     bygender_df.rename(columns={
         "customer_id": "customer_count"
     }, inplace=True)
-    
     return bygender_df
 
 def create_byage_df(df):
@@ -37,7 +35,6 @@ def create_byage_df(df):
         "customer_id": "customer_count"
     }, inplace=True)
     byage_df['age_group'] = pd.Categorical(byage_df['age_group'], ["Youth", "Adults", "Seniors"])
-    
     return byage_df
 
 def create_bystate_df(df):
@@ -45,12 +42,11 @@ def create_bystate_df(df):
     bystate_df.rename(columns={
         "customer_id": "customer_count"
     }, inplace=True)
-    
     return bystate_df
 
 def create_rfm_df(df):
     rfm_df = df.groupby(by="customer_id", as_index=False).agg({
-        "order_date": "max", #mengambil tanggal order terakhir
+        "order_date": "max",  # mengambil tanggal order terakhir
         "order_id": "nunique",
         "total_price": "sum"
     })
@@ -60,7 +56,6 @@ def create_rfm_df(df):
     recent_date = df["order_date"].dt.date.max()
     rfm_df["recency"] = rfm_df["max_order_timestamp"].apply(lambda x: (recent_date - x).days)
     rfm_df.drop("max_order_timestamp", axis=1, inplace=True)
-    
     return rfm_df
 
 all_df = pd.read_csv("all_data.csv")
@@ -68,24 +63,20 @@ all_df = pd.read_csv("all_data.csv")
 datetime_columns = ["order_date", "delivery_date"]
 all_df.sort_values(by="order_date", inplace=True)
 all_df.reset_index(inplace=True)
- 
+
 for column in datetime_columns:
     all_df[column] = pd.to_datetime(all_df[column])
 
 min_date = all_df["order_date"].min()
 max_date = all_df["order_date"].max()
- 
+
 with st.sidebar:
-    # Menambahkan logo perusahaan
     st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
-    
-    # Mengambil start_date & end_date dari date_input
     start_date, end_date = st.date_input(
-        label='Rentang Waktu',min_value=min_date,
+        label='Rentang Waktu', min_value=min_date,
         max_value=max_date,
         value=[min_date, max_date]
     )
-
 
 main_df = all_df[(all_df["order_date"] >= str(start_date)) & 
                 (all_df["order_date"] <= str(end_date))]
@@ -130,14 +121,14 @@ fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35, 15))
  
 colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
  
-sns.barplot(x="quantity_x", y="product_name", data=sum_order_items_df.head(5), palette=colors, ax=ax[0])
+sns.barplot(x="quantity", y="product_name", data=sum_order_items_df.head(5), palette=colors, ax=ax[0])
 ax[0].set_ylabel(None)
 ax[0].set_xlabel("Number of Sales", fontsize=30)
 ax[0].set_title("Best Performing Product", loc="center", fontsize=50)
 ax[0].tick_params(axis='y', labelsize=35)
 ax[0].tick_params(axis='x', labelsize=30)
  
-sns.barplot(x="quantity_x", y="product_name", data=sum_order_items_df.sort_values(by="quantity_x", ascending=True).head(5), palette=colors, ax=ax[1])
+sns.barplot(x="quantity", y="product_name", data=sum_order_items_df.sort_values(by="quantity", ascending=True).head(5), palette=colors, ax=ax[1])
 ax[1].set_ylabel(None)
 ax[1].set_xlabel("Number of Sales", fontsize=30)
 ax[1].invert_xaxis()
@@ -208,7 +199,6 @@ st.pyplot(fig)
 st.subheader("Best Customer Based on RFM Parameters")
  
 col1, col2, col3 = st.columns(3)
- 
 with col1:
     avg_recency = round(rfm_df.recency.mean(), 1)
     st.metric("Average Recency (days)", value=avg_recency)
